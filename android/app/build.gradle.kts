60bf721ef613586bab3e1bb1338f6a72b40f9835
Parksh <2171164@hansung.ac.kr>
2025-09-11 15:56:24 +0900

    🐛 버전 충돌 해결

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,10 @@ plugins {
 android {
     namespace = "com.example.frontend"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    
+
+    //명시적으로 버전 고정
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -24,7 +27,7 @@ android {
         applicationId = "com.example.frontend"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 24  // Required by flutter_sound
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
